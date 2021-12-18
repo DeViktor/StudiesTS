ec365a65076e0f7794af735e2807eb2d65ab9297
DeViktor <contato.4years@gmail.com>
2021-12-18 14:51:20 -0400

    add alert

---
+++ alert/scripts/alert.ts
@@ -0,0 +1,8 @@
+const btnAlert = document.getElementById('alert')
+
+btnAlert.addEventListener('click', () => {
+    window.alert('You Clicked on Alert Button')
+})
+// window.addEventListener('DOMContentLoaded', () => {
+//     toggleButton();
+// });
